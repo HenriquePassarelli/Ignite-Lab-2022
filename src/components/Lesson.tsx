c8adb3e505d1a38b4d65a70880abbf0940a55766
Henrique Passarelli <henrique.passarellicc@gmail.com>
2022-06-23 23:31:04 -0300

    aula 3 done

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBr from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom'
 interface ILessonProps {
     title: string
     slug: string
@@ -15,11 +16,11 @@ export const Lesson = (props: ILessonProps) => {
     })
 
     return (
-        <a href="#" className="">
+        <Link to={`/event/lesson/${props.slug}`} className="group">
             <span className="text-gray-300">
                 {availableDateFormatted}
             </span>
-            <div className="rounded border border-gray-500 p-4 mt-2">
+            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
                 <header className="flex items-center justify-between">
                     {isLessonAvailable ?
                         (<span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
@@ -39,6 +40,6 @@ export const Lesson = (props: ILessonProps) => {
                     {props.title}
                 </strong>
             </div>
-        </a>
+        </Link>
     )
 }
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,18 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, DefaultOptions, InMemoryCache } from '@apollo/client'
+
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+        fetchPolicy: "no-cache",
+        errorPolicy: "ignore"
+    },
+    query: {
+        fetchPolicy: "no-cache",
+        errorPolicy: "all"
+    }
+}
 
 export const client = new ApolloClient({
     uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o8xb8t1c6y01z21add0xlh/master',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    defaultOptions
 })
