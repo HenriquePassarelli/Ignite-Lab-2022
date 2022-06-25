953e4c23354ce18ccf79d27832c2194ca753f2e7
Henrique Passarelli Cugini Campos <71354731+HenriquePassarelli@users.noreply.github.com>
2022-06-24 23:59:06 -0300

    Update apollo.ts

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o8xb8t1c6y01z21add0xlh/master',
+    uri: import.meta.env.VITE_API_URL,
     cache: new InMemoryCache()
 })
